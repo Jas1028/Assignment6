7c587722ccc64447902a0b54e0a7735b9fe5be0b
Justine Charles T. Simbahon <92497072+Jas1028@users.noreply.github.com>
2021-11-27 08:08:57 +0800

    Made second level

--- Program_2_Addition_Trainer.py
+++ Program_2_Addition_Trainer.py
@@ -9,9 +9,6 @@
 # It will generate two random numbers
 import random
 
-
-
-
 # In this game, there are 10 levels.
 GameLevel = 10
 #Your score will increase if you will answer it correctly.
@@ -24,18 +21,36 @@ Value2 = random.randint(0,99)
 
 print("Level 1")
 
-Level1 = int(input(f"Sum of {Value1} and {Value2}.\nEnter your answer: " ))
+Level1 = int(input(f"\nSum of {Value1} and {Value2}.\nEnter your answer: " ))
 CorrectAnswer = Value1 + Value2
 
 if CorrectAnswer == Level1:
     print("\nLevel 1 passed. ")
     Score = Score + 1
-    print(f"Your score is {Score}. ")
+    print(f"Your current score is {Score}. ")
 else:
     CorrectAnswer != Level1
     print("Level 1 failed. ")
-    print(f"Your score is {Score}. ")
+    print(f"Your current score is {Score}. ")
+
+Score = Score
+
+Value3 = random.randint(0,99)
+Value4 = random.randint(0,99)
 
 
+print("\n\nLevel 2")
 
+Level2 = int(input(f"\nSum of {Value3} and {Value4}.\nEnter your answer: " ))
+CorrectAnswer = Value3 + Value4
+
+if CorrectAnswer == Level2:
+    print("\nLevel 2 passed. ")
+    Score = Score + 1
+    print(f"Your current score is {Score}. ")
+else:
+    CorrectAnswer != Level2
+    print("Level 2 failed. ")
+    print(f"Your current score is {Score}. ")
+    
 
